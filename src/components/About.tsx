32e6e5478e71463e9d9b9e96cdf2aef1ed0e3f56
Raghavendra singh <raghvandrasingh111@gmail.com>
2026-05-18 19:22:35 +0530

    About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,9 +6,13 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quis
-          dolores numquam iusto Ratione earum ducimus autem id iure pariatur
-          dolorum quae maiores.
+          I'm a passionate Full Stack Web & App Developer currently pursuing my 3rd year at Chandigarh University. I specialize in building modern, fast, and user-friendly digital experiences for web and mobile platforms.
+
+          Currently, I’m working as a developer with Mairal Holidays Private Limited and Luxury Trip India, where I develop scalable websites and applications focused on performance, responsiveness, and seamless user experience.
+
+          My expertise includes frontend and backend development, mobile app development, and creating premium UI/UX experiences for brands and businesses. I work with technologies like Flutter Framework, React, TypeScript, HTML, and CSS to deliver high-quality digital products.
+
+          I’m passionate about turning ideas into powerful digital solutions and continuously learning new technologies to stay ahead in the development industry.
         </p>
       </div>
     </div>
